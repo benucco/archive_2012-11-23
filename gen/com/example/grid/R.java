19f83a837cc90a7a6dad59aa678a052b8e0c76af
student <student@comp001-09.itstep1.dp.ua>
2012-11-23 19:32:23 +0200

    Добавил картинки в drawable-mdpi

--- gen/com/example/grid/R.java
+++ gen/com/example/grid/R.java
@@ -11,11 +11,33 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int app_sample_code=0x7f020000;
+        public static final int asteroid02=0x7f020001;
+        public static final int asteroid03=0x7f020002;
+        public static final int asteroid04=0x7f020003;
+        public static final int asteroid07=0x7f020004;
+        public static final int asteroid08=0x7f020005;
+        public static final int asteroid09=0x7f020006;
+        public static final int asteroid10=0x7f020007;
+        public static final int asteroid_explode2=0x7f020008;
+        public static final int asteroid_explode4=0x7f020009;
+        public static final int ff=0x7f02000a;
+        public static final int ic_action_search=0x7f02000b;
+        public static final int ic_home=0x7f02000c;
+        public static final int ic_launcher=0x7f02000d;
+        public static final int ic_menu_preferences=0x7f02000e;
+        public static final int pause=0x7f02000f;
+        public static final int pause_pressed=0x7f020010;
+        public static final int play=0x7f020011;
+        public static final int play_pressed=0x7f020012;
+        public static final int rew=0x7f020013;
+        public static final int rew_pressed=0x7f020014;
+        public static final int stop=0x7f020015;
+        public static final int stop_pressed=0x7f020016;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int gridView1=0x7f070000;
+        public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
